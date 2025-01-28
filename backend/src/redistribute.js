349be07a3ff0605611b304addbaae8bf96563cff
graduaition <bycocoaz@gmail.com>
2025-01-28 20:06:33 +0100

    Create redistribute.js

---
+++ backend/src/redistribute.js
@@ -0,0 +1,10 @@
+const Solana = require('@solana/web3.js');
+
+async function redistributeGains(winner) {
+    // Fetch bets from db
+    // Calculate gains
+    // Redistributes funds
+    // Save 1% fees to buy $graduation
+}
+
+module.exports = { redistributeGains };
